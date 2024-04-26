bdadfd53123997a01f3a9d9e2fa01d0fca4ddb4e
francktshibala <franck1tshibala@gmail.com>
2024-04-26 17:39:21 -0600

    updated to homepage

---
+++ scripts/getdates.js
@@ -0,0 +1,6 @@
+const currentYear = new Date().getFullYear();
+document.getElementById('copyright-year').textContent = currentYear;
+
+
+const lastModified = document.lastModified;
+document.getElementById('last-modified').textContent = lastModified;
