f75a89f7bf5814ca089cef742dfabb77a41fe2d9
francktshibala <franck1tshibala@gmail.com>
2024-05-04 23:14:02 -0600

    updated to home page

--- scripts/temples.js
+++ scripts/temples.js
@@ -1,7 +1,23 @@
+
+
+
+// DATE AND TIME 
+// 
+const currentYear = new Date().getFullYear();
+document.getElementById('copyright-year').textContent = currentYear;
+
+
+const lastModified = document.lastModified;
+document.getElementById('last-modified').textContent = lastModified;
+//  Hamburger Menu 
+
 const hamButton = document.querySelector('#menu');
 const navigation = document.querySelector('.navigation');
 
-hamButton.addEventListener('_______', () => {
-	navigation._______.toggle('_______');
-	hamButton.classList.toggle('open');
+hamButton.addEventListener('click', () => {
+    navigation.classList.toggle('open');
+    hamButton.classList.toggle('open');
+    document.querySelector('.close-icon').classList.toggle('show');
 });
+ 
+ 
