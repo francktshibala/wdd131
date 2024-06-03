465052ee0a2788895cd1d3856ec4eea310ccb2f8
francktshibala <franck1tshibala@gmail.com>
2024-06-03 00:26:30 -0600

    update to home page

---
+++ scripts/advanced.js
@@ -0,0 +1,18 @@
+//Hamburger Menu 
+
+const mainnav = document.querySelector('.navigation')
+const hambutton = document.querySelector('#menu')
+
+hambutton.addEventListener('click', () => {
+  mainnav.classList.toggle('show');
+  hambutton.classList.toggle('show');
+});
+
+// DATE AND TIME 
+// 
+const currentYear = new Date().getFullYear();
+document.getElementById('copyright-year').textContent = currentYear;
+
+
+const lastModified = document.lastModified;
+document.getElementById('last-modified').textContent = lastModified;
